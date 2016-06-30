7049d171842e42e5e2622f948f41e8d16e41499a
Artur <aavkhadi@gmail.com>
2016-06-30 13:19:06 +0200

    finished the script, renamed to count_estimate.py

--- CMSSW_8_0_11/src/RatesAnalysis/RateEstimate/count_estimate.py
+++ CMSSW_8_0_11/src/RatesAnalysis/RateEstimate/count_estimate.py
@@ -16,8 +16,14 @@
 ## To get the total number of events, we need to sum the number of entries across all the files. 
 ## To get the total number of events accepted by the trigger, we need to sum the number of entries across all the files such that their attribute for the given trigger's name is 1.
 #
-# Pseudocode:
-## import ROOT
+
+# force non-truncating division to avoid converting ints to floats
+from __future__ import division
+# importing decimal for a neater work with decimal numbers (instead of floats)
+from decimal import Decimal
+# importing csv to write the counts in the file
+import csv
+
 import ROOT
 
 # import libraries to parse through directories and find the required files
@@ -39,9 +45,11 @@ def findFiles (path, filter):
 trigger_list = ["HLT_DoubleJetsC100_DoubleBTagCSV_p014_DoublePFJetsC100MaxDeta1p6_v2",
 	"HLT_DoubleJetsC100_DoubleBTagCSV_p026_DoublePFJetsC160_v2",
 	"HLT_PFJet80_v5",
-	"HLT_DiPFJetAve80_v3"]
+	"HLT_DiPFJetAve80_v3"
+	]
+
 ## Specify the directory where the histrograms (*.root reside)
-directory = "" 
+dataset_directory = "/afs/cern.ch/user/a/aavkhadi/commissioning_trigger_bjet/rate_studies/CMSSW_8_0_11/src/tests/crabjobs" 
 ## Specify a filter for finding *.root files in the directory (and subdirectories)
 events_file_filter = "hltbits*.root"
 
@@ -49,49 +57,58 @@ events_file_filter = "hltbits*.root"
 ### trigger names from the list as keys, and 0 as values. 
 events_accepted = dict.fromkeys(trigger_list, 0)
 
-## Create a dictionary of all rates, rates
+## Create a dictionary of all counts, counts
 ### trigger names from the list as keys, and NaN as values.
-rates = dict.fromkeys(trigger_list, None)
+counts = dict.fromkeys(trigger_list, None)
 
 ## Declare a variable for a total number of events, events_total = 0
 events_total = 0
   
 ## Loop over all directories
 try:			
-	for events_file in findFiles(directory, events_file_filter)
+	for events_file in findFiles(dataset_directory, events_file_filter):
+		print ("Going through the file " + events_file)
 		#### Open the file in ROOT: event_file = ROOT.
 		events_file_ROOT = ROOT.TFile(events_file)
-		#### Copy the total number of events in the file:
-		events_in_file = events_file_ROOT.GetEntries()
-		#### Increment events_total by events_in_file
-		events_total += events_in_file
 		#### Copy the tree from the file: trigger_tree = ROOT.gDirectory.Get("HltTree")
 		trigger_tree = ROOT.gDirectory.Get("HltTree") # relies on the fact that all trees in files are named "HltTree"
+		#### Copy the total number of events in the file:
+                events_in_file = trigger_tree.GetEntries()
+                print ("Number of events in the file is " + str(events_in_file))
+                #### Increment events_total by events_in_file
+                events_total += events_in_file
 		#### Loop over the trigger names in accepted_events
-			for trigger_name in trigger_list 
-				##### Try to get the number of events in the file accepted by a given trigger: 
-				accepted_by_trigger_in_file = 0
-				##### Loop over all events in the file: 
-				for event in trigger_tree
-				###### If event was accepted by the trigger, increment the counter: 
-					try:	
-						if getattr(event,trigger_name) == 1:
-							accepted_by_trigger_in_file += 1
-					except:
-						##### Exception: If couldn't find the trigger_name in the tree,
-						##### print out that trigger was not found in the menu
-					 	err_notrigger = "Couldn't find the trigger " + trigger_name + " in the file " + str(events_file) 
-						print err_notrigger
-				events_accepted[trigger_name] += accepted_by_trigger_in_file
-
-## Loop over the rates dictionary. Compute the rate for a given trigger as the respective value from events_accepted divided by event_total. 
-if events_total != 0
-	for trigger_name in trigger_list
-		rates[trigger_name] = events_accepted[trigger_name]/events_total
-		# Print out the rates dictionary
-		print trigger_name, rates[trigger_name]
-elif events_total == 0
-	err_noevents = "Couldn't find any events"
-	print err_noevents
-
-# Test Cases
+		for trigger_name in trigger_list:
+			##### Try to get the number of events in the file accepted by a given trigger: 
+			print ("Working with the trigger " + trigger_name) 
+			accepted_by_trigger_in_file = 0
+			##### Loop over all events in the file: 
+			for event in trigger_tree:
+			###### If event was accepted by the trigger, increment the counter: 
+				try:	
+					if getattr(event,trigger_name) == 1:
+						accepted_by_trigger_in_file += 1
+				except:
+					##### Exception: If couldn't find the trigger_name in the tree,
+					##### print out that trigger was not found in the menu
+				 	err_notrigger = "Couldn't find the trigger " + trigger_name + " in the file " + str(events_file) 
+					print err_notrigger
+			print ("Number of events accepted by the trigger in the file is "+ str(accepted_by_trigger_in_file))
+			events_accepted[trigger_name] += accepted_by_trigger_in_file
+	
+	print ("Total number of events is " + str(events_total)
+	## Loop over the counts dictionary. Compute the count for a given trigger as the respective value from events_accepted divided by event_total. 
+	if events_total != 0:
+		counts_writeout = csv.writer(open('counts.csv', 'wb'))	
+		print "Writing the counts in a file counts.csv..."
+		for trigger_name, count in counts.items():
+                	count = events_accepted[trigger_name]/events_total
+                	# Print out the counts dictionary, display in scientific notation
+			# see http://stackoverflow.com/questions/6913532/display-a-decimal-in-scientific-notation
+                	count_display = '%.2E' % Decimal(str(count))
+			counts_writeout.writerow([trigger_name, count_display])	
+	elif events_total == 0:
+        	err_noevents = "Couldn't find any events" 
+        	print err_noevents
+except:
+	print "Something went wrong when going through *.root files"
